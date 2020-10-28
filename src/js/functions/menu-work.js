130bba7a60d5410d81d0083b17e4fc421540d7d0
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2020-10-28 18:26:50 +0300

    add disable scroll in menu

--- src/js/functions/menu-work.js
+++ src/js/functions/menu-work.js
@@ -1,13 +1,15 @@
-const menu = document.querySelector('.nav')
-const media = document.querySelector('.media')
-const menuOpen = document.querySelector('.menu-open-btn')
-const menuClose = document.querySelector('.menu-close')
+const menu = document.querySelector('.nav');
+const media = document.querySelector('.media');
+const menuOpen = document.querySelector('.menu-open-btn');
+const menuClose = document.querySelector('.menu-close');
+const body = document.body;
 
 menuOpen.addEventListener('click', function(e) {
   e.preventDefault();
   menu.classList.add('menu-open'); 
   media.classList.add('menu-open'); 
   menuClose.classList.add('menu-open'); 
+  body.classList.add('disable-scroll');
 });
 
 menuClose.addEventListener('click', function(e) {
@@ -15,4 +17,5 @@ menuClose.addEventListener('click', function(e) {
   menu.classList.remove('menu-open'); 
   media.classList.remove('menu-open'); 
   menuClose.classList.remove('menu-open'); 
+  body.classList.remove('disable-scroll');
 }); 
